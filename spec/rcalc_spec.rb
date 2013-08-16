32394b72ac1963646cd6b2a0ebf1a8f68939e343
Jack Christensen <jack@jackchristensen.com>
2013-08-16 16:48:57 -0500

    Add failing spec for parenthesis order of operations

--- spec/rcalc_spec.rb
+++ spec/rcalc_spec.rb
@@ -45,4 +45,9 @@ describe Rcalc do
   it 'raises error on evil input' do
     expect{rcalc.compute 'exit 1'}.to raise_error(ArgumentError)
   end
+
+  it 'computes order of operations with parenthesis' do
+    answer = rcalc.compute '(1 + 1) * 2'
+    expect(answer).to eq 4
+  end
 end
