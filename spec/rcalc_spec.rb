5f2577207271f0fea588890e4a5aaa388cb92729
Jack Christensen <jack@jackchristensen.com>
2013-08-16 14:55:22 -0500

    Added failing addition test

--- spec/rcalc_spec.rb
+++ spec/rcalc_spec.rb
@@ -6,4 +6,10 @@ describe Rcalc do
     rcalc = Rcalc.new
     expect(rcalc.compute('1')).to eq 1
   end
+
+  it 'computes addition' do
+    rcalc = Rcalc.new
+    answer = rcalc.compute '1 + 1'
+    expect(answer).to eq 2
+  end
 end
