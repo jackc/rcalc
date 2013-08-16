45ef60e639958af2bc9d68d555b69a3bac50649c
Jack Christensen <jack@jackchristensen.com>
2013-08-16 15:05:08 -0500

    DRY Rcalc spec with let

--- spec/rcalc_spec.rb
+++ spec/rcalc_spec.rb
@@ -2,19 +2,18 @@ require 'rspec'
 require 'rcalc'
 
 describe Rcalc do
+  let(:rcalc) { Rcalc.new }
+
   it 'computes an integer' do
-    rcalc = Rcalc.new
     expect(rcalc.compute('1')).to eq 1
   end
 
   it 'computes addition of integers' do
-    rcalc = Rcalc.new
     answer = rcalc.compute '1 + 1'
     expect(answer).to eq 2
   end
 
   it 'computes subtraction of integers' do
-    rcalc = Rcalc.new
     answer = rcalc.compute '2 - 1'
     expect(answer).to eq 1
   end
