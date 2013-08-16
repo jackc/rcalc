116bd4264ef1094a8067746bdeedf05d73d87901
Jack Christensen <jack@jackchristensen.com>
2013-08-16 16:38:52 -0500

    Added order of operations support
    But did it by introducing a scary eval

--- lib/rcalc.rb
+++ lib/rcalc.rb
@@ -1,22 +1,7 @@
 class Rcalc
   def compute(expression)
-    tokens = tokenize(expression)
-    if tokens.size == 1
-      tokens.first.to_i
-    else
-      left = tokens[0].to_i
-      operand = tokens[1]
-      right = tokens[2].to_i
-
-      if %w[+ - * /].include? operand
-        left.send(operand, right)
-      else
-        raise ArgumentError
-      end
-    end
-  end
-
-  def tokenize(expression)
-    expression.scan /\d+|[+\-*\/]/
+    eval expression
+  rescue SyntaxError
+    raise ArgumentError
   end
 end
