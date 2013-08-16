57c69cffea83bebf9e864075985ddec739580ea2
Jack Christensen <jack@jackchristensen.com>
2013-08-16 16:47:35 -0500

    Sanitize input

--- lib/rcalc.rb
+++ lib/rcalc.rb
@@ -1,7 +1,14 @@
 class Rcalc
   def compute(expression)
+    sanitize! expression
     eval expression
   rescue SyntaxError
     raise ArgumentError
+  end
+
+  private
+
+  def sanitize! expression
+    raise ArgumentError unless expression =~ /\A[ 0-9+\-*\/]+\Z/
   end
 end
